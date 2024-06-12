196a65c6f097eace36d4b0448c20ad2b2d272bc3
Pinki Kumari <160412282+pinkimandal44066@users.noreply.github.com>
2024-06-12 19:47:03 +0530

    her

--- src/Components/Homefolder/Onepagescrolling.jsx
+++ src/Components/Homefolder/Onepagescrolling.jsx
@@ -111,27 +111,27 @@ function App() {
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
       {/* <img class="d-block w-100" src={img1} alt="First slide"> */}
       <img src={img9} alt="" />
     </div>
-    <div class="carousel-item">
+    <div className="carousel-item">
       {/* <img class="d-block w-100" src={img9} alt="Second slide"> */}
       <img src={img10} alt="" />
     </div>
-    <div class="carousel-item">
+    <div className="carousel-item">
       {/* <img class="d-block w-100" src={img10}alt="Third slide"> */}
       <img src={img1} alt="" />
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
+  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
+  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
   </a>
 </div>
           
